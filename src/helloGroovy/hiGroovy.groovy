60321f70f74bf8759d6794e4e09c6a395d653150
Ajeet Yadav <38289540+ajitjss@users.noreply.github.com>
2018-12-01 07:55:41 +0530

    initial commit

---
+++ src/helloGroovy/hiGroovy.groovy
@@ -0,0 +1,10 @@
+package helloGroovy
+
+class hiGroovy {
+
+	  static void main(String[] args) {
+      // Using a simple println statement to print output to the console
+      println('Hello World');
+   }
+	
+}
---
+++ src/helloGroovy/varibables.groovy
@@ -0,0 +1,18 @@
+package helloGroovy
+
+class varibables {
+	static main(args) {
+		
+		def a=67
+		def b=67.99877
+		def c= a+b
+		println(c)
+		// lets check looping 
+		def i=0;
+		while(i <=8){
+			println("QA EduPoint"+i)
+			i++
+		}
+	}
+
+}
